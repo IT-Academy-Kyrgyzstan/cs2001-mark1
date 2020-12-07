58053b2e5fd78e60af4a9ce1785fadd017b0e229
Doron Doron <doronkg@yandex.ru>
2020-12-07 15:07:01 +0600

    faet: Create Product Table

---
+++ DataAccess/ContextApp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    class ContextApp : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ContextApp()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+            => builder.UseSqlServer(@"Server=DORON-PC\SQLEXPRESS; Database=Mark1; Trusted_Connection=True;");
+    }
+}
